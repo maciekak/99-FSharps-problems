9ae01fdeea9f8f7734ddbebbe7ce7b2924f49bee
maciej.klimowski <maciej.klimowski@geomatyka-krakow.pl>
2018-08-10 14:47:41 +0200

    Done second problem

---
+++ Problem_2_PreLastElList/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec preLastEl aList =
+    match aList with
+    | (a :: _ :: []) -> a
+    | (_ :: b :: al) -> preLastEl (b :: al)
+    | _ -> failwith "Not enought length of list"
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (preLastEl [1; 2; 3; 4; 5;])
+    printfn "%A" (preLastEl [1; 2;])
+
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
