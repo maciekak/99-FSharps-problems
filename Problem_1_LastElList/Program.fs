8e43da0aba55fbeff6a77dfa0a2f70071a65b4ec
maciej.klimowski <maciej.klimowski@geomatyka-krakow.pl>
2018-08-10 14:38:49 +0200

    Done first problem

---
+++ Problem_1_LastElList/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let testList = [1; 2; 3; 4; 5; 6]
+
+let rec lastEl aList =
+    match aList with
+    | (a :: []) -> a
+    | (_ :: al) -> lastEl al
+    | _ -> failwith "Not supported size of list"
+
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (lastEl testList)
+    printfn "%A" (lastEl [1])
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
