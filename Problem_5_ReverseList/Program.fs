329fa32f0acb15cb1f7ef4ee92a3311bb0d976d9
maciej.klimowski <maciej.klimowski@geomatyka-krakow.pl>
2018-08-13 17:09:50 +0200

    Problem 5 done

---
+++ Problem_5_ReverseList/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let reverseList aList = 
+    let rec helperReverse original result =
+        match original with
+        | [] -> result
+        | a :: al -> helperReverse al (a :: result)
+    helperReverse aList []
+    
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (reverseList [1; 2; 3; 4; 5; 6;])
+    printfn "%A" (reverseList [1])
+    printfn "%A" (reverseList [])
+
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
