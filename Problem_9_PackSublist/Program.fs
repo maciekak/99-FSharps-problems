e87022a7243a373b9b6b8e03a4c9dddd18b19afe
maciej.klimowski <maciej.klimowski@geomatyka-krakow.pl>
2018-08-16 17:36:01 +0200

    Problem 9 done

---
+++ Problem_9_PackSublist/Program.fs
@@ -0,0 +1,22 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let pack lst = 
+    let rec packHelper lt acc last = 
+        match (lt, last) with
+        | ((a :: ax), last) when a = last -> packHelper ax (a :: acc) a
+        | ((a :: ax), _) -> acc :: packHelper ax [a] a
+        | ([], _) -> [acc]
+    match lst with
+    | (a :: ax) -> packHelper ax [a] a
+    | [] -> [[]]
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (pack [1; 1; 2; 2; 3; 4; 4; 4; 4])
+    printfn "%A" (pack [1])
+    printfn "%A" (pack [])
+    printfn "%A" (pack [1; 1])
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
