be54ddb36235aa0c534c293c36e9238eb14bd9c3
maciej.klimowski <maciej.klimowski@geomatyka-krakow.pl>
2018-08-16 17:21:22 +0200

    Done Problem 8

---
+++ Problem_8_EliminateDuplicates/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let compress lst = 
+    let rec compressHelper lt last =
+        match (lt, last) with
+        | ((a :: ax), last) when last = a -> compressHelper ax a
+        | ((a :: ax), _) -> a :: compressHelper ax a
+        | ([], _) -> []
+    match lst with
+    | (b :: bx) -> b :: compressHelper bx b
+    | [] -> []
+    
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (compress [1; 1; 2; 2; 3; 4; 4; 4; 4])
+    printfn "%A" (compress [1])
+    printfn "%A" (compress [])
+    printfn "%A" (compress [1; 1])
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
