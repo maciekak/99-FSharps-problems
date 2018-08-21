8d4e76faef88dcf8876cb7682afc382d2109b731
maciej.klimowski <maciej.klimowski@geomatyka-krakow.pl>
2018-08-21 17:21:23 +0200

    Done problem 14

---
+++ Problem_14_DuplicateListsElem/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec dupli lst = 
+    match lst with
+    | (a :: ax) -> a :: a :: dupli ax
+    | [] -> []
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (dupli [1; 2; 3;])
+    printfn "%A" (dupli [1])
+    printfn "%A" (dupli [])
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
