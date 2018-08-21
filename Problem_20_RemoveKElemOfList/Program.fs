90c64780d9fa42b6d1e7c69c64b2329767f5a4eb
maciej.klimowski <maciej.klimowski@geomatyka-krakow.pl>
2018-08-21 18:04:28 +0200

    Done problem 20

---
+++ Problem_20_RemoveKElemOfList/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec removeAt lst at = 
+    match (lst, at) with
+    | ((_ :: ax), index) when index = 1 -> ax
+    | ((a :: ax), index) -> a :: removeAt ax (index - 1)
+    | _ -> []
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (removeAt [1; 2; 3; 4; 5; 6; 7; 8; 9;] 9)
+    printfn "%A" (removeAt [1; 2; 3; 4; 5; 6; 7; 8; 9;] 10)
+    printfn "%A" (removeAt [1; 2; 3; 4; 5; 6; 7; 8; 9;] 4)
+    printfn "%A" (removeAt [1; 2; 3; 4; 5; 6; 7; 8; 9;] 1)
+    printfn "%A" (removeAt [1] 2)
+    printfn "%A" (removeAt [] 2)
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
