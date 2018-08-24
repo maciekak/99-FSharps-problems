d928d61648bbf35c61c28939cca7f1a3140957c7
maciej.klimowski <maciej.klimowski@geomatyka-krakow.pl>
2018-08-24 16:23:47 +0200

    Done problem 22

---
+++ Problem_22_ListAtRange/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec range beg fin = 
+    match (beg, fin) with
+    | (b, f) when b = f -> [f]
+    | (b, f) -> b :: range (b+1) f
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (range 2 2)
+    printfn "%A" (range 2 5)
+    printfn "%A" (range 1 2)
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
