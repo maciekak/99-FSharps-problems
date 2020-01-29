23b105f4c66dfe33144ab7c018c399fb49e1d230
maciekak <maciek.klimowski44@gmail.com>
2020-01-29 17:58:59 +0100

    Done problem 32

---
+++ Problem_32_GreatestCommonDivisor/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+let rec gcd fst snd =
+    match (fst, snd) with
+    | (fst, snd) when snd = 0 -> fst
+    | (fst, snd) -> gcd snd (fst % snd)
+let res fst snd = 
+    printfn "(%d %d) -> %d" fst snd (gcd fst snd)
+
+[<EntryPoint>]
+let main argv =
+    res 5 15
+    res 15 5
+    res 140 187
+    res 6 49
+    res 12 123
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
