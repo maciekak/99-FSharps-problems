aada9d092d703fcff9c57ea12cb73289b1612ebd
Maciej Klimowski <maciek.klimowski44@gmail.com>
2020-03-14 01:08:11 +0100

    Beginging with 49

---
+++ Problem_49_Gray/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let gray n =
+    
+
+[<EntryPoint>]
+let main argv =
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
